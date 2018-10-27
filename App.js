a26250ed57a44c726313a99a0820f2e6369e0a0d
dbfarms <matt.hunger@gmail.com>
2018-10-27 16:23:00 -0400

    watch position works

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
+import MapView, { Marker, AnimatedRegion, Polyline } from "react-native-maps";
+import haversine from "haversine";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      latitude: undefined,
+      longitude: undefined,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {}, /*
+      coordinate: new AnimatedRegion({
+        latitude: this.state.latitude,
+        longitude: this.state.longitude
+      })*/
+    }
+  }
+
+  componentDidMount(){
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        const { coordinate, routeCoordinates, distanceTravelled } = this.state;
+        const { latitude, longitude } = position.coords;
+
+        const newCoordinate = {
+          latitude,
+          longitude
+        };
+
+        /*
+        if (Platform.OS === "android") {
+          if (this.marker) {
+            this.marker._component.animateMarkerToCoordinate(
+              newCoordinate,
+              500
+            );
+          }
+        } else {
+          coordinate.timing(newCoordinate).start();
+        }
+        */
+
+        this.setState({
+          latitude,
+          longitude, /*
+          routeCoordinates: routeCoordinates.concat([newCoordinate]),
+          distanceTravelled:
+            distanceTravelled + this.calcDistance(newCoordinate),
+          prevLatLng: newCoordinate */
+        });
+      },
+      error => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>lat: {this.state.latitude} and lng: {this.state.longitude}</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
